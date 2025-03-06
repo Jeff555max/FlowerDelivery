ed0b4d4f37c065faaa54885ea43acdc42006bf53
Jeff555max <vadik98765@gmail.com>
2025-03-06 14:41:48 +0500

    3

--- website/shop/admin.py
+++ website/shop/admin.py
@@ -6,9 +6,6 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'created_at')
     search_fields = ('name',)
 
-
-admin.site.register(Product)
-
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'phone', 'total_price', 'status', 'created_at')
@@ -18,4 +15,3 @@ class OrderAdmin(admin.ModelAdmin):
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ('session_key', 'product', 'quantity')
-
