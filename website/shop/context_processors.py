4b6b9b2867b5209696a2ff047087e73f7bd8869f
Jeff555max <vadik98765@gmail.com>
2025-03-08 17:34:28 +0500

    12

--- website/shop/context_processors.py
+++ website/shop/context_processors.py
@@ -1,10 +1,10 @@
 from .models import Cart
 
 def cart_count(request):
-    """Добавляет количество товаров в корзине в контекст шаблона."""
+    """Передает количество товаров в корзине во все шаблоны"""
     session_key = request.session.session_key
     if not session_key:
         return {'cart_count': 0}
 
-    count = Cart.objects.filter(session_key=session_key).count()
-    return {'cart_count': count}
+    cart_items_count = Cart.objects.filter(session_key=session_key).count()
+    return {'cart_count': cart_items_count}
